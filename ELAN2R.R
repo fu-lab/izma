a0517c7d4f3441cbfeebeb10c9968053120c9030
nikopartanen <nikotapiopartanen@gmail.com>
2014-08-22 21:46:12 +0300

    лутче сегментация

---
+++ ELAN2R.R
@@ -0,0 +1,17 @@
+library(XML)
+xmlfiles_all <- list.files(path=".", pattern="*.eaf$", recursive=TRUE)
+xmlfiles <- xmlfiles_all[ !grepl("kpv_novyje",xmlfiles_all) ]
+n <- length(xmlfiles)
+dat <- vector("list", n)
+for(i in 1:n){
+        doc <- xmlTreeParse(xmlfiles[i], useInternalNodes = TRUE)
+        nodes <- getNodeSet(doc, "//TIER[@LINGUISTIC_TYPE_REF='wordT']")
+        x<- lapply(nodes, function(x){ data.frame(
+                Filename = xmlfiles[i],
+                Speaker= xpathSApply(x, "." , xmlGetAttr, "PARTICIPANT"),
+                Word= xpathSApply(x, ".//ANNOTATION/REF_ANNOTATION/ANNOTATION_VALUE" , xmlValue) )})
+        dat[[i]] <- do.call("rbind", x)
+}
+kpv_corpus <- do.call("rbind", dat)
+
+write.csv(kpv_corpus, "kpv_corpus.csv")
