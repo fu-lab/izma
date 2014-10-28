32b3a301fda54cd3fa4e3b0ba493fc01abd72d7b
nikopartanen <nikotapiopartanen@gmail.com>
2014-10-28 16:18:24 +0100

    новый экспорт

--- meta/export_process.R
+++ meta/export_process.R
@@ -1,4 +1,5 @@
 #Export.csv has been exported from FileMaker Pro from Sessions-view with search Session name=*izva* and Github=T
+setwd("~/Desktop/github/data/izma/meta")
 library(plyr)
 eaf.meta <- read.csv("export.csv", header=F)
 eaf.meta <- rename(eaf.meta, c("V1" = "файл", "V2" = "год", "V3" = "сегментаций", "V4" = "расшифровка", "V5" = "русский", "V6" = "английский", "V7" = "спеллер", "V8" = "просмотр"))
