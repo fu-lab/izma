2ed296e22271269fbda065302b130670e2b35f63
nikopartanen <nikotapiopartanen@gmail.com>
2014-10-30 12:38:41 +0100

    писал немного с английском что сделает

--- ELAN2R.R
+++ ELAN2R.R
@@ -1,4 +1,18 @@
-# Тайӧ скрипт лыддьӧ ЕЛАН-файлъяс кыв-тиеръясті. Йитча сылӧн дата фрамесӧ менам Филемакер метаданныекӧд.
+# Тайӧ скрипт лыддьӧ ЕЛАН-файлъяс кыв-тиеръясті. Йитча сылӧн дата фрамесӧ менам 
+# Филемакер метаданныекӧд. This script reads the content of ELAN files that are 
+# in folders under it. It reads only the tiers with the linguistic type wordT, 
+# and it joins this data with participant attributes for those tiers + 
+# filenames. The script doesn't assume very much about the data, but it requires
+# that each word-tier has a participant and that each word-tier has at least one
+# annotation. The folder structure is such that the files that do not fill these
+# criteria are in the folder kpv_novyje, content of that file is excluded from 
+# the object xmlfiles_all. I often use this script from another script with 
+# commands like "source("./../ELAN2R.R")" This play with the setting of working 
+# directory is just one way to keep stuff running, but ideally there would be no
+# parameters that are somehow speficied to my computer. The idea is that the
+# filenames correspond exactly with your session names. This way it is possible
+# to connect participant and session specific metadata with the content of
+# word-tiers.
 
 setwd("~niko/Desktop/github/data/izma")
 
