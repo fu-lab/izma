b7b918e96ceb0cc6bb61932043078abe3d34e221
nikopartanen <nikotapiopartanen@gmail.com>
2014-10-30 12:37:24 +0100

    видео колонна

--- meta/export_process.R
+++ meta/export_process.R
@@ -2,8 +2,8 @@
 setwd("~/Desktop/github/data/izma/meta")
 library(plyr)
 eaf.meta <- read.csv("export.csv", header=F)
-eaf.meta <- rename(eaf.meta, c("V1" = "файл", "V2" = "год", "V3" = "сегментаций", "V4" = "расшифровка", "V5" = "русский", "V6" = "английский", "V7" = "спеллер", "V8" = "просмотр"))
+eaf.meta <- rename(eaf.meta, c("V1" = "файл", "V2" = "год", "V3" = "сегментаций", "V4" = "расшифровка", "V5" = "русский", "V6" = "английский", "V7" = "спеллер", "V8" = "просмотр", "V9" = "видео?"))
 library(dplyr)
 eaf.meta <- eaf.meta %>%
         arrange(файл)
-write.csv(eaf.meta, file="meta-export.csv")
+write.csv(eaf.meta, file="metadannye_izva.csv")
